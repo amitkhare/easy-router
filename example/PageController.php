c1004a0bd14fd1b236802081fd40ea8beae43e8c
Amit Kumar Khare <amitkhare@users.noreply.github.com>
2017-07-14 16:17:47 +0530

    Create PageController.php

---
+++ example/PageController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controllers;
+
+class PageController {
+	
+	public function __construct($vars=[]) {
+		foreach ($vars as $key => $value) {
+			$this->$key= $value;
+		}
+	}
+	public function get($vars)
+	{
+		echo $this->db."<br/>";
+		echo $this->var1."<br/>";
+		echo $this->var1."<br/>";
+		print_r($vars);
+	}
+	public function about($vars)
+	{
+		echo $vars->var1."<br/>";
+		echo $vars->myname."<br/>";
+	}
+	public function product($vars)
+	{
+		echo " ID: ".$vars->id;
+	}
+	public function home()
+	{
+		echo "this is home.";
+	}
+	public function product_process()
+	{
+		echo "this will show only if accessed via POST method.";
+	}
+}
